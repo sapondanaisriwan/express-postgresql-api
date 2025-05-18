b5cf8fc75f08926955178865fc5e72a4529223e8
Sapondanai Sriwan <sapondanaisriwan@gmail.com>
2025-05-18 19:10:01 +0700

    refactor: organize imports and streamline server setup in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,35 @@ import express from "express";
 import cors from "cors";
 import pool from "./config/db";
 
+import userRoute from "./routes/user.route";
+import errorHandling from "./middlewares/errorHandler";
+import { createUserTable } from "./data/createUserTable";
+
 const app = express();
 const port = process.env.PORT || 3001;
 
-console.log(typeof process.env.DB_PORT)
-
 // Middleware
-// app.use(express.json());
-// app.use(cors());
+app.use(express.json());
+app.use(cors());
 
-// // Routes
-// app.get("/", async (req, res) => {
-//   const result = await pool.query("SELECT current_database()");
-//   res.send(`Hello world: ${result.rows[0].current_database}`);
-// });
+// Create table before starting server
+createUserTable();
 
-// // Error handling middleware
+// Routes
+app.use("/api", userRoute);
 
-// // Server running
-// app.listen(port, () => {
-//   console.log(`Server is running at port ${port}`);
-// });
+// Error handling middleware for above routes
+app.use(errorHandling);
+
+// Testing POSTGRES connection
+app.get("/", async (req, res) => {
+  const result = await pool.query("SELECT current_database()");
+  res.send(`Hello world: ${result.rows[0].current_database}`);
+});
+
+// Error handling middleware
+
+// Server running
+app.listen(port, () => {
+  console.log(`Server is running at port ${port}`);
+});
